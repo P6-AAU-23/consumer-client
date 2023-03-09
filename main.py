9b0fd120ed147466490c70874577604dbaa49fb0
Casper Ståhl <cstahl20@student.aau.dk>
2023-03-09 14:35:08 +0100

    Create a very simple rtmp client

--- main.py
+++ main.py
@@ -1,22 +1,10 @@
-import os
-import socketserver
+import cv2
 
-class MyUDPHandler(socketserver.BaseRequestHandler):
-    """
-    This class works similar to the TCP handler class, except that
-    self.request consists of a pair of data and client socket, and since
-    there is no connection the client address must be given explicitly
-    when sending data back via sendto().
-    """
+myrtmp_addr = "rtmp://172.17.0.2:1935/live/test"
+cap = cv2.VideoCapture(myrtmp_addr)
 
-    def handle(self):
-        data = self.request[0].strip()
-        socket = self.request[1]
-        print("{} wrote:".format(self.client_address[0]))
-        print(data)
-        socket.sendto(data.upper(), self.client_address)
+print(cap.read())
 
-if __name__ == "__main__":
-    HOST, PORT = os.environ["ip"], int(os.environ["port"])
-    with socketserver.UDPServer((HOST, PORT), MyUDPHandler) as server:
-        server.serve_forever()
+
+
+
