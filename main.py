ea1617b3c3dfed787129ac84dc3984dc5a974f16
Casper Ståhl <cstahl20@student.aau.dk>
2023-03-13 10:59:59 +0100

    Server now shows stream

--- main.py
+++ main.py
@@ -1,10 +1,30 @@
+import sys
 import cv2
+import argparse
 
-myrtmp_addr = "rtmp://172.17.0.2:1935/live/test"
-cap = cv2.VideoCapture(myrtmp_addr)
+def main():
+	parser = argparse.ArgumentParser()
+	parser.add_argument('video_capture_address') 
+	args = parser.parse_args()
+	cap = cv2.VideoCapture(args.video_capture_address)
+	if not cap.isOpened():
+		print("Can't open camera")
+		exit()
+	while True:
+		ret, frame = cap.read()
+		if not ret:
+			print("Can't receive frame (stream end?). Exiting ...")
+			break
+		whiteboard = idealize(frame)
+		cv2.imshow('preview', whiteboard)
+		if cv2.waitKey(1) == ord('q'):
+			break
+	cap.release()
+	cv2.destroyAllWindows()
 
-print(cap.read())
+# TODO: implement pipeline here
+def idealize(frame):
+	return frame
 
-
-
-
+if __name__ == "__main__":
+	main()
