afb8b05b3fc1873a8f98920b0c8a42c0648d86f5
Casper Ståhl <cstahl20@student.aau.dk>
2023-03-14 08:46:29 +0100

    Change tabs to 4 spaces

--- main.py
+++ main.py
@@ -2,28 +2,28 @@ import cv2
 import argparse
 
 def main():
-	parser = argparse.ArgumentParser()
-	parser.add_argument('video_capture_address') 
-	args = parser.parse_args()
-	cap = cv2.VideoCapture(args.video_capture_address)
-	if not cap.isOpened():
-		print("Can't open camera")
-		exit()
-	while True:
-		ret, frame = cap.read()
-		if not ret:
-			print("Can't receive frame (stream end?). Exiting ...")
-			break
-		whiteboard = idealize(frame)
-		cv2.imshow('preview', whiteboard)
-		if cv2.waitKey(1) == ord('q'):
-			break
-	cap.release()
-	cv2.destroyAllWindows()
+    parser = argparse.ArgumentParser()
+    parser.add_argument('video_capture_address') 
+    args = parser.parse_args()
+    cap = cv2.VideoCapture(args.video_capture_address)
+    if not cap.isOpened():
+        print("Can't open camera")
+        exit()
+    while True:
+        ret, frame = cap.read()
+        if not ret:
+            print("Can't receive frame (stream end?). Exiting ...")
+            break
+        whiteboard = idealize(frame)
+        cv2.imshow('preview', whiteboard)
+        if cv2.waitKey(1) == ord('q'):
+            break
+    cap.release()
+    cv2.destroyAllWindows()
 
 # TODO: implement pipeline here
 def idealize(frame):
-	return frame
+    return frame
 
 if __name__ == "__main__":
-	main()
+    main()
