143cfec89692b88a228b49cb930a0bcbf70ae562
Casper Ståhl <cstahl20@student.aau.dk>
2023-03-18 01:00:01 +0100

    Document quadrilateral_to_rectangle

--- helper.py
+++ helper.py
@@ -1,10 +1,19 @@
+from typing import List, Tuple
 import numpy as np
 import cv2
 
 def distance(pt1: tuple, pt2: tuple) -> float:
     return np.sqrt((pt1[0] - pt2[0]) ** 2 + (pt1[1] - pt2[1]) ** 2)
 
-def quadrilateral_to_rectangle(image, corners):
+def quadrilateral_to_rectangle(image: np.ndarray, corners: List[Tuple[int, int]]) -> np.ndarray:
+    """
+    Warps a quadrilateral region in the input image into a rectangular shape using a perspective transformation.
+
+    :param image: The input image.
+    :param corners: A list of 4 corner points defining the quadrilateral region in the input image.
+                    The order of corner points should be: upper-left, upper-right, lower-right, lower-left.
+    :return: The output image with the quadrilateral region warped into a rectangular shape.
+    """
     width_upper = distance(corners[0], corners[1])
     width_lower = distance(corners[2], corners[3])
     max_width = int(max(width_upper, width_lower))
@@ -17,6 +26,6 @@ def quadrilateral_to_rectangle(image, corners):
             (max_width - 1, max_height - 1),
             (0, max_height - 1)
         ], dtype=np.float32)
-    H = cv2.getPerspectiveTransform(np.float32(corners), target_corners) # type: ignore
-    out = cv2.warpPerspective(image, H, (max_width, max_height)) # type: ignore 
+    H = cv2.getPerspectiveTransform(np.float32(corners), target_corners)  # type: ignore
+    out = cv2.warpPerspective(image, H, (max_width, max_height))  # type: ignore
     return out
