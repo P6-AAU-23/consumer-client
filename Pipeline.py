1929820eebbe0553419af61cfd1aa109c547f7f3
CasperStaahl <cstahl20@student.aau.dk>
2023-03-20 16:49:26 +0100

    Implement, document, test and benchmark the quad to square perspective transformation and associated GUI
    * Ignore invalid cv2 errors
    
    * PerspectiveTransformer GUI now works
    
    * Implement PerspectiveTransformer
    
    * Fix scaling bug
    
    * Something is not working
    
    * Now works, but can't not multithreaded
    
    * Cleanup and Refactor
    
    * Remove corner_provider.stop() from stop()
    
    * Add documentation to CornerProvider.py
    
    * Import Any from typing
    
    * Document quadrilateral_to_rectangle
    
    * Refactor Pipeline.py
    
    * Refactor and update documentation
    
    * Add tests and update documentation
    
    * Fix test when no window manager was available
    
    * Fix an off by one error
    
    * Fix another off by one error
    
    * Fixed the last off by one error for real now
    
    * Create benchmark

---
+++ Pipeline.py
@@ -0,0 +1,17 @@
+import cv2
+
+from CornerProvider import CornerProvider
+from helper import quadrilateral_to_rectangle
+
+class Pipeline():
+    def __init__(self, args):
+        self.corner_provider = CornerProvider('Corner Selection Preview')
+        self.args = args
+
+    def process(self, image):
+        self.corner_provider.update(image)
+        corners = self.corner_provider.get_corners()
+        whiteboard = quadrilateral_to_rectangle(image, corners)
+        return whiteboard
+
+        
--- main.py
+++ main.py
@@ -1,29 +1,28 @@
 import cv2
 import argparse
 
+from Pipeline import Pipeline
+
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('video_capture_address') 
     args = parser.parse_args()
-    cap = cv2.VideoCapture(args.video_capture_address)
+    cap = cv2.VideoCapture(args.video_capture_address) # type: ignore
+    pipeline = Pipeline(args)
     if not cap.isOpened():
         print("Can't open camera")
         exit()
     while True:
-        ret, frame = cap.read()
+        ret, image = cap.read()
         if not ret:
             print("Can't receive frame (stream end?). Exiting ...")
             break
-        whiteboard = idealize(frame)
-        cv2.imshow('preview', whiteboard)
-        if cv2.waitKey(1) == ord('q'):
+        whiteboard = pipeline.process(image)
+        cv2.imshow('preview', whiteboard) # type: ignore
+        if cv2.waitKey(1) == ord('q'): # type: ignore
             break
     cap.release()
-    cv2.destroyAllWindows()
-
-# TODO: implement pipeline here
-def idealize(frame):
-    return frame
+    cv2.destroyAllWindows() # type: ignore
 
 if __name__ == "__main__":
     main()
