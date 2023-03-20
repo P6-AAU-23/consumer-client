47a816e2bd16eb54d1363f19d9221615dace2ed9
thom776g <thomas@nordfiber.dk>
2023-03-20 10:54:07 +0100

    Calling SegmentationRemover, and cleaned up our imports

--- MaskingPipeline/CaptureActivity.py
+++ MaskingPipeline/CaptureActivity.py
@@ -3,11 +3,7 @@ import cv2 as cv
 import numpy as np
 import time
 
-from MaskingPipeline.Pipeline import Binarization
-from MaskingPipeline.Pipeline import Changes
-from MaskingPipeline.Pipeline import Colourization
-from MaskingPipeline.Pipeline import Segmentation
-from MaskingPipeline.Pipeline import UpdateWhiteboard
+from MaskingPipeline.Pipeline import Binarization, Changes, Colourization, Segmentation, UpdateWhiteboard, SegmentationRemoval
 
 class CaptureActivity:
         
@@ -27,6 +23,7 @@ class CaptureActivity:
         
         segImg = Segmentation.SegmentAct(img)
         binImg = Binarization.BinarizeAct(greyScaled)
+        yes = SegmentationRemoval.RemoveSegmentAct(binImg, segImg)
         TrackedChangesMask = Changes.ChangesAct(binImg, segImg)
         ColouredChanges = Colourization.ColouringAct(TrackedChangesMask, origImg)
         self.VirtualWhiteboard = UpdateWhiteboard.UpdateWhiteboardAct(ColouredChanges, self.VirtualWhiteboard)
