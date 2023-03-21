5ba93c1bed64bb0175b722c8337b8a8ac507cfb8
thom776g <thomas@nordfiber.dk>
2023-03-21 11:27:05 +0100

    Restructuring of TestMain to run tests better and smoother
    -SegmentRemoval Test written
    -GetPath function
    -WholePipelineTest written

--- TestMain.py
+++ TestMain.py
@@ -2,14 +2,41 @@ import cv2 as cv
 import os
 from pathlib import Path
 from MaskingPipeline.CaptureActivity import CaptureActivity
+from MaskingPipeline.Pipeline.SegmentationRemoval import RemoveSegmentAct
 
-ROOT_DIR = os.path.realpath(os.path.dirname(__file__))
-relativePath = Path('Tests/Images')
-fullPath = ROOT_DIR / relativePath / 'whiteboard1.png'
+def GetPath():
+    ROOT_DIR = os.path.realpath(os.path.dirname(__file__))
+    relativePath = Path('Tests/Images')
+    return ROOT_DIR / relativePath 
 
-print(fullPath)
 
-image = cv.imread(str(fullPath))
 
-CA = CaptureActivity(image)
-CA.CaptureActivity(image)
+
+def RemoveSegmentFromBinaryTest():
+    path = GetPath()
+    segPath = path / 'segTest.png'
+    binPath = path / 'binTest.png'
+
+    seg = cv.imread(str(segPath))
+    bin = cv.imread(str(binPath))
+
+    segGrey = cv.cvtColor(seg, cv.COLOR_BGR2GRAY)
+    binGrey = cv.cvtColor(bin, cv.COLOR_BGR2GRAY)
+
+    segBin = cv.adaptiveThreshold(segGrey, 255, cv.ADAPTIVE_THRESH_GAUSSIAN_C, cv.THRESH_BINARY, 1501, 4)
+    binBin = cv.adaptiveThreshold(binGrey, 255, cv.ADAPTIVE_THRESH_GAUSSIAN_C, cv.THRESH_BINARY_INV, 21, 4)
+
+    segbin = RemoveSegmentAct(binBin, segBin)
+
+    cv.imshow('test', segbin)
+    cv.waitKey(0)
+
+def WholePipelineTest():
+    fullPath = GetPath() / 'whiteboard1.png'
+    image = cv.imread(str(fullPath))
+
+    CA = CaptureActivity(image)
+    CA.CaptureActivity(image)
+
+WholePipelineTest()
+RemoveSegmentFromBinaryTest()
