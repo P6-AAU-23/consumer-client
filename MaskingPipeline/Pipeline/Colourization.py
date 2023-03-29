cd33e711faeb7890e16d2176a14a00c79e0576a3
thom776g <thomas@nordfiber.dk>
2023-03-29 15:58:21 +0200

    Fixed Colourisation

--- MaskingPipeline/Pipeline/Colourization.py
+++ MaskingPipeline/Pipeline/Colourization.py
@@ -3,6 +3,6 @@ import cv2 as cv
 
 def ColouringAct(Mask, OrigImg):
 
-    colouredChanges = cv.bitwise_and(Mask, OrigImg)
+    colouredChanges = cv.bitwise_and(OrigImg, OrigImg, mask=Mask)
 
     return colouredChanges
