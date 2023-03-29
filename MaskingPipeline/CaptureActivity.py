661927c1cb729106b67b038ab3e83a6567a76d4b
thom776g <thomas@nordfiber.dk>
2023-03-29 15:55:25 +0200

    Rename of CaptureActivity and inclusion of  segmentation
    Also included terminal arguments for test

--- MaskingPipeline/CaptureActivity.py
+++ MaskingPipeline/CaptureActivity.py
@@ -1,28 +1,30 @@
 import cv2 as cv
 import numpy as np
 import time
+import torch
 
 from MaskingPipeline.Pipeline import Binarization, Changes, Colourization, Segmentation, UpdateWhiteboard, SegmentationRemoval
+
 
 class CaptureActivity:
         
     VirtualWhiteboard = None
+    Seg = Segmentation.Segmentator()
 
     def __init__(self, img):
         self.VirtualWhiteboard = np.full(img.shape, 255, dtype=np.uint8)
 
-    def CaptureActivity(self, img):
+    def CaptureActivityAct(self, img):
         timestamp = time.time()
 
         origImg = img
         
-        greyScaled = cv.cvtColor(origImg, cv.COLOR_BGR2GRAY)
-        
-        segImg = Segmentation.SegmentAct(img)
-        binImg = Binarization.BinarizeAct(greyScaled)
+        binImg = Binarization.BinarizeAct(img)
+        segImg = self.Seg.SegmentAct(img)
+
         removedSegBinImg = SegmentationRemoval.RemoveSegmentAct(binImg, segImg)
         TrackedChangesMask = Changes.ChangesAct(binImg, segImg)
         ColouredChanges = Colourization.ColouringAct(TrackedChangesMask, origImg)
         self.VirtualWhiteboard = UpdateWhiteboard.UpdateWhiteboardAct(ColouredChanges, self.VirtualWhiteboard)
 
-        print((time.time()-timestamp)*1000)
+        print((time.time()-timestamp))
--- Tests/WholePipelineTest.py
+++ Tests/WholePipelineTest.py
@@ -10,12 +10,19 @@ sys.path.append(PROJECT_ROOT)
 from MaskingPipeline.CaptureActivity import CaptureActivity
 from Tests.testFunctions import GetPath
 
-def WholePipelineTest():
-    fullPath = GetPath() / 'whiteboard1.png'
-    image = cv.imread(str(fullPath))
+def main():
+    arg = sys.argv[1:]
+    WholePipelineTest(arg[0])
 
-    CA = CaptureActivity(image)
-    CA.CaptureActivity(image)
+
+def WholePipelineTest(imgName):
+    path = GetPath()
+    imgName += '.jpg'
+    fullPath = path / imgName
+    img = cv.imread(str(fullPath))
+
+    CA = CaptureActivity(img)
+    CA.CaptureActivityAct(img)
 
 if __name__ == '__main__':   
-    WholePipelineTest()
+    main()
