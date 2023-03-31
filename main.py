b91214d163f33ab1e96b759bc7f0aec3a79458d7
thom776g <thomas@nordfiber.dk>
2023-03-31 10:06:56 +0200

    Save image upon closing

--- main.py
+++ main.py
@@ -1,14 +1,23 @@
 import cv2
 import argparse
+import os
+from pathlib import Path
 
+from Tests.testFunctions import GetPath
 from Pipeline import Pipeline
 
 
 def main():
+    PROJECT_ROOT = os.path.abspath(os.path.join(os.path.dirname(__file__)))
+    print(PROJECT_ROOT)
+    relative_path = Path("Tests/Results/")
+    image_path = PROJECT_ROOT / relative_path
     parser = argparse.ArgumentParser()
     parser.add_argument("video_capture_address")
+    parser.add_argument("final_whiteboard_image_path", nargs="?", default=image_path)
     args = parser.parse_args()
     cap = cv2.VideoCapture(args.video_capture_address)  # type: ignore
+
     pipeline = Pipeline()
     if not cap.isOpened():
         print("Can't open camera")
@@ -22,7 +31,12 @@ def main():
         whiteboard = pipeline.process(image)
         cv2.imshow("preview", whiteboard)  # type: ignore
         if cv2.waitKey(1) == ord("q"):  # type: ignore
+            path = Path(args.final_whiteboard_image_path) / 'whiteboard.jpg'
+            print(path)
+            cv2.imwrite(str(path), whiteboard)
             break
+
+
     cap.release()
     cv2.destroyAllWindows()  # type: ignore
 
