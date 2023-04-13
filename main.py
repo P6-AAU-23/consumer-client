87c525b165dde950bd9350943554f8380966d164
thom776g <thomas@nordfiber.dk>
2023-04-13 10:37:45 +0200

    Implementation of Whiteboard class
    The latest whiteboard now has its own class, so it is more accessible in multiple files

--- main.py
+++ main.py
@@ -1,39 +1,45 @@
 import cv2
 import argparse
 import os
+from CurrentWhiteboard import CurrentWhiteboard
 from pathlib import Path
 from helper import uniquifyFileName
 from BufferlessVideoCapture import BufferlessVideoCapture
 from Pipeline import Pipeline
 
 
+
 def main():
     PROJECT_ROOT = os.path.abspath(os.path.join(os.path.dirname(__file__)))
-    print(PROJECT_ROOT)
     relative_path = Path("Tests/Results/")
     image_path = PROJECT_ROOT / relative_path
     parser = argparse.ArgumentParser()
     parser.add_argument("--video_capture_address", nargs="?", default=0)
-    parser.add_argument("--saved_path", nargs="?", default=image_path)
+    parser.add_argument("--save_path", nargs="?", default=image_path)
     args = parser.parse_args()
     cap = BufferlessVideoCapture(args.video_capture_address)  # type: ignore
     pipeline = Pipeline()
+    latestWhiteboard = CurrentWhiteboard()
+
     # TODO: this should probably implemented for the BufferlessVideoCapture, and uncommented
     # if not cap.isOpened():
     #     print("Can't open camera")
     #     exit()
     while True:
         image = cap.read()
+
         # TODO: this should probably implemented for the BufferlessVideoCapture, and uncommented
         # if not ret:
         #     print("Can't receive frame (stream end?). Exiting ...")
         #     break
-        whiteboard = pipeline.process(image)
-        cv2.imshow("preview", whiteboard)  # type: ignore
+        latestWhiteboard.setWhiteboard(pipeline.process(image))
+        
+        cv2.imshow("preview", latestWhiteboard.getWhiteboard())  # type: ignore
         if cv2.waitKey(1) == ord("q"):  # type: ignore
-            path = Path(args.saved_path) / "whiteboard.jpg"
-            path = uniquifyFileName(path)
-            cv2.imwrite(str(path), whiteboard)
+            path = Path(args.save_path) 
+            #print()
+            #print(path)
+            latestWhiteboard.saveWhiteboard(path, "closing_whiteboard.jpg")
             break
 
     # TODO: this should probably implemented for the BufferlessVideoCapture, and uncommented
