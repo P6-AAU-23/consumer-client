1f65e596925acf0eac0318e9df412352f65cdef9
Casper Ståhl <cstahl20@student.aau.dk>
2023-04-13 10:35:18 +0200

    Move Inpainter unit tests to Inpainter.py

--- src/pipeline/Inpainter.py
+++ src/pipeline/Inpainter.py
@@ -39,3 +39,46 @@ class Inpainter:
         inpainted_image = cv2.add(masked_input, masked_last_image)  # type: ignore
         self._last_image = inpainted_image
         return inpainted_image
+
+
+def test_inpaint_missing_inpaints_missing():
+    # Arrange
+    input = np.ones((100, 100, 3), dtype=np.uint8) * 255
+    cv2.rectangle(input, (25, 25), (74, 74), (255, 0, 0), -1)  # type: ignore
+
+    expected = np.ones((100, 100, 3), dtype=np.uint8) * 255
+    cv2.rectangle(expected, (25, 25), (74, 74), (0, 255, 0), -1)  # type: ignore
+
+    last_image = expected.copy()
+
+    mask = np.ones((100, 100, 1), dtype=np.uint8) * 255
+    cv2.rectangle(mask, (25, 25), (74, 74), 0, -1)  # type: ignore
+
+    inpainter = Inpainter()
+    inpainter._last_image = last_image
+
+    # Act
+    actual = inpainter.inpaint_missing(input, mask)
+
+    # Assert
+    assert np.array_equal(actual, expected)
+
+
+def test_inpaint_missing_does_not_inpaint_if_no_missing():
+    # Arrange
+    input = np.ones((100, 100, 3), dtype=np.uint8) * 255
+    cv2.rectangle(input, (25, 25), (74, 74), (255, 0, 0), -1)  # type: ignore
+
+    last_image = np.ones((100, 100, 3), dtype=np.uint8) * 255
+    cv2.rectangle(last_image, (25, 25), (74, 74), (0, 255, 0), -1)  # type: ignore
+
+    mask = np.ones((100, 100, 1), dtype=np.uint8) * 255
+
+    inpainter = Inpainter()
+    inpainter._last_image = last_image
+
+    # Act
+    actual = inpainter.inpaint_missing(input, mask)
+
+    # Assert
+    assert np.array_equal(actual, input)
