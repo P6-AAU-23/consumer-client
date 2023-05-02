8991b71fdc9fbbf5522d742d5238b81d8e59d2b3
Casper Ståhl <cstahl20@student.aau.dk>
2023-05-02 10:20:44 +0200

    It works :)

--- src/pipeline/pipeline.py
+++ src/pipeline/pipeline.py
@@ -113,7 +113,12 @@ def scale_saturation(image: np.ndarray, amount: float) -> np.ndarray:
 
 
 def fullness(image: np.ndarray) -> int:
-    binary_image = binarize(image)
+    image_grey = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)  # type: ignore
+    binary_image = cv2.adaptiveThreshold(  # type: ignore
+        image_grey, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 21, 4  # type: ignore
+    )
+    binary_image = cv2.medianBlur(binary_image, 3)  # type: ignore
+    binary_image = cv2.bitwise_not(binary_image)  # type: ignore
     return np.count_nonzero(binary_image == 255)
 
 
