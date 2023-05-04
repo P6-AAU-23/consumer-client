d683980a4fb7ff498dc1ea941b4551619f61f78f
thom776g <thomas@nordfiber.dk>
2023-05-04 09:02:53 +0200

    Wench the Bench is back

--- benches/pipeline.py
+++ benches/pipeline.py
@@ -27,26 +27,6 @@ pipeline = Pipeline()
                 """,
             number=SCALE,
         )
-
-        sum_time1 = timeit.timeit(
-            """
-self.corner_provider.update(image)
-corners =self.corner_provider.get_corners()
-whiteboard = quadrilateral_to_rectangle(image, corners)
-cv2.imshow("benchmark", whiteboard)
-cv2.waitKey(1)
-                """,
-            setup=f"""
-import sys
-sys.path.append("{project_root}")
-from src.pipeline.corner_provider import CornerProvider
-import cv2
-gc.enable()
-image = cv2.imread("{project_root}/resources/benchmark{i}.jpg")
-corner_provider = CornerProvider("test")
-                """,
-            number=SCALE,
-        )
         
         actual_fps = SCALE / sum_time
         average_time = sum_time / SCALE
@@ -56,8 +36,3 @@ corner_provider = CornerProvider("test")
         )
         print(f"FPS: {actual_fps}.")
         print(f"time: {average_time}.")
-
-        step_1_fps = SCALE / sum_time1
-        step_1_time = sum_time1 / SCALE
-        print(f"mhm testing:{step_1_fps}, {step_1_time}")
-        
--- benches/theWrench.py
+++ benches/theWrench.py
@@ -5,31 +5,185 @@ import sys
 
 
 from pathlib import Path
-path_root = Path(__file__).parents[1]
+path_root = os.getcwd()
 sys.path.append(str(path_root))
 from src.pipeline.pipeline import Pipeline
 from src.pipeline.pipeline import Pipeline
 
 SCALE = 1
 
-pipeline = Pipeline()
+#pipeline = Pipeline()
     # timeit.repeat(stmt='pass', setup='pass', timer=<default timer>, repeat=5, number=1000000, globals=None)
 
-def mockImages():
-    image = []
+if __name__ == "__main__":
+    project_root = os.getcwd()
+    for i in range(3):
+        sum_time = timeit.timeit(
+            """
+whiteboard = pipeline.corner_provider.update(image)
+corners = pipeline.corner_provider.get_corners()
+whiteboard = quadrilateral_to_rectangle(image, corners)
+cv2.imshow("benchmark", whiteboard)
+cv2.waitKey(1)
+                """,
+            setup=f"""
+import sys
+sys.path.append("{project_root}")
+from src.pipeline.pipeline import Pipeline, quadrilateral_to_rectangle
+import cv2
+gc.enable()
+image = cv2.imread("{project_root}/resources/benchmark{i}.jpg")
+pipeline = Pipeline()
+                """,
+            number=SCALE,
+        )
+        print()
+        print()
+
+        actual_fps = SCALE / sum_time
+        average_time = sum_time / SCALE
+        height, _, _ = cv2.imread(f"resources/benchmark{i}.jpg").shape  # type: ignore
+        print(
+            f"-------------------------quadrilateral_to_rectangle {height}p-------------------------"
+        )
+        print(f"FPS: {actual_fps}.")
+        print(f"time: {average_time}.")
+
+    for i in range(3):
+        sum_time = timeit.timeit(
+            """
+foreground_mask = pipeline.foreground_remover.segment(whiteboard)
+cv2.imshow("benchmark", foreground_mask)
+cv2.waitKey(1)
+                """,
+            setup=f"""
+import sys
+sys.path.append("{project_root}")
+from src.pipeline.pipeline import Pipeline, quadrilateral_to_rectangle
+import cv2
+gc.enable()
+image = cv2.imread("{project_root}/resources/benchmark{i}.jpg")
+pipeline = Pipeline()
+whiteboard = pipeline.corner_provider.update(image)
+corners = pipeline.corner_provider.get_corners()
+whiteboard = quadrilateral_to_rectangle(image, corners)
+                """,
+            number=SCALE,
+        )
+        print()
+        print()
+
+        actual_fps = SCALE / sum_time
+        average_time = sum_time / SCALE
+        height, _, _ = cv2.imread(f"resources/benchmark{i}.jpg").shape  # type: ignore
+        print(
+            f"-------------------------segmentation {height}p-------------------------"
+        )
+        print(f"FPS: {actual_fps}.")
+        print(f"time: {average_time}.")
+
+    for i in range(3):
+        sum_time = timeit.timeit(
+            """
+whiteboard = pl.idealize_colors(whiteboard, pl.IdealizeColorsMode.MASKING)
+cv2.imshow("benchmark", whiteboard)
+cv2.waitKey(1)
+                """,
+            setup=f"""
+import sys
+sys.path.append("{project_root}")
+from src.pipeline import pipeline as pl
+from src.pipeline.pipeline import Pipeline
+import cv2
+gc.enable()
+image = cv2.imread("{project_root}/resources/benchmark{i}.jpg")
+pipeline = Pipeline()
+whiteboard = pipeline.corner_provider.update(image)
+corners = pipeline.corner_provider.get_corners()
+whiteboard = pl.quadrilateral_to_rectangle(image, corners)
+foreground_mask = pipeline.foreground_remover.segment(whiteboard)
+                """,
+            number=SCALE,
+        )
+        print()
+        print()
+
+        actual_fps = SCALE / sum_time
+        average_time = sum_time / SCALE
+        height, _, _ = cv2.imread(f"resources/benchmark{i}.jpg").shape  # type: ignore
+        print(
+            f"-------------------------idealize_colors {height}p-------------------------"
+        )
+        print(f"FPS: {actual_fps}.")
+        print(f"time: {average_time}.")
+
+    for i in range(3):
+        sum_time = timeit.timeit(
+            """
+whiteboard = pipeline.inpainter.inpaint_missing(whiteboard, foreground_mask)
+cv2.imshow("benchmark", whiteboard)
+cv2.waitKey(1)
+                """,
+            setup=f"""
+import sys
+sys.path.append("{project_root}")
+from src.pipeline import pipeline as pl
+from src.pipeline.pipeline import Pipeline
+import cv2
+gc.enable()
+image = cv2.imread("{project_root}/resources/benchmark{i}.jpg")
+pipeline = Pipeline()
+whiteboard = pipeline.corner_provider.update(image)
+corners = pipeline.corner_provider.get_corners()
+whiteboard = pl.quadrilateral_to_rectangle(image, corners)
+foreground_mask = pipeline.foreground_remover.segment(whiteboard)
+whiteboard = pl.idealize_colors(whiteboard, pl.IdealizeColorsMode.MASKING)
+                """,
+            number=SCALE,
+        )
+        print()
+        print()
+
+        actual_fps = SCALE / sum_time
+        average_time = sum_time / SCALE
+        height, _, _ = cv2.imread(f"resources/benchmark{i}.jpg").shape  # type: ignore
+        print(
+            f"-------------------------inpaint_missing {height}p-------------------------"
+        )
+        print(f"FPS: {actual_fps}.")
+        print(f"time: {average_time}.")
+
+#To-do make work
+def benchmark(name, test, setup, SCALE):
+    for i in range(3):
+        sum_time = timeit.timeit(test, setup=setup, number=SCALE)
+        
+        actual_fps = SCALE / sum_time
+        average_time = sum_time / SCALE
+        height, _, _ = cv2.imread(f"resources/benchmark{i}.jpg").shape  # type: ignore
+        print(
+            f"-------------------------{name} {height}p-------------------------"
+        )
+        print(f"FPS: {actual_fps}.")
+        print(f"time: {average_time}.")
+
+
+# def mockImages():
+#     image = []
     
-    for i in range(0,3):
-       image.append(cv2.imread(f"resources/benchmark{i}.jpg"))
-    return image
+#     for i in range(0,3):
+#        image.append(cv2.imread(f"resources/benchmark{i}.jpg"))
+#     return image
 
 
-def otherBench():
-    for img in mockImages():
-        whiteboard = pipeline.process(img)
-        cv2.imshow("benchmark", whiteboard)
-        cv2.waitKey(1)
-    return whiteboard
+# def otherBench():
+#     for img in mockImages():
+#         whiteboard = pipeline.process(img)
 
-otherBench()
+#         cv2.imshow("benchmark", whiteboard)
+#         cv2.waitKey(1)
+#     return whiteboard
 
-timeit.repeat(otherBench, setup="pass", repeat=1, number=SCALE)
+# otherBench()
+
+# timeit.repeat(otherBench, setup="pass", repeat=1, number=SCALE)
